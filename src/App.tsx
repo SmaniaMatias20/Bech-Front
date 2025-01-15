b87a28d25e58552000223236ec8221606dfd7d34
Matias <matiasezequielsmania@gmail.com>
2025-01-15 15:30:52 -0500

    commit de prueba

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+      <Button variant="destructive" size="lg">Delete</Button>
+    </div>
+
+  )
+}
+
+export default App
