f897fec2f222e7afe60893634542886019b7474b
Matias <matiasezequielsmania@gmail.com>
2025-01-19 18:21:55 -0300

    fin de la pagina de prueba

--- src/components/custom/SectorsSection.tsx
+++ src/components/custom/SectorsSection.tsx
@@ -10,13 +10,17 @@ interface SectorsSectionProps {
 const SectorsSection: React.FC<SectorsSectionProps> = ({ title }) => {
     return (
         <section className="flex flex-col p-4 gap-4">
-            <div className='flex justify-between items-center'>
-                <h2 className='text-xl text-gray-500'>{title}</h2>
-                <Button className='bg-sky-800 text-white rounded-xl'>Explorar sectores</Button>
+            <div className="flex justify-between items-center">
+                <h2 className="text-xl text-gray-500">{title}</h2>
+                <Button className="bg-sky-800 hover:bg-sky-900 text-white rounded-xl">
+                    Explorar sectores
+                </Button>
             </div>
-            <div className="flex bg-white  border-2 border-gray-200 rounded-xl p-4 gap-4 overflow-x-auto">
-                {sectors.map((sector) => (
+            {/* Apply responsive flex direction */}
+            <div className="flex bg-white border-2 border-gray-200 rounded-xl p-4 gap-4 overflow-x-auto md:flex-row flex-col">
+                {sectors.map((sector, index) => (
                     <CardSector
+                        key={index}
                         title={sector.title}
                         time={sector.time}
                         tasks={sector.tasks}
